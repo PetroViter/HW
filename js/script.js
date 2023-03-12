2555c75610de5c66808bf2137341076ee7aecd70
PetroViter <116358398+PetroViter@users.noreply.github.com>
2023-03-12 10:04:50 +0200

    +

--- js/script.js
+++ js/script.js
@@ -1,26 +1,9 @@
-// task #1
-console.log(Boolean(Number('10') + 1 )) //true
-console.log('sub ' + Number(false)) //sub 0
-console.log(16 * `     91        `)// 1456
-console.log(true-70)//-69
-console.log(Number(1 + String(1)) + 1) // 12
-
-
-//Task #2
-const secondInHours = 60*60;
-const hour = +prompt('введите количество часов');
-const result = hour*secondInHours;
-alert(`В ${hour} часах ${result} секунд`);
-
-
-//Task #3
 let num = 1;
-num += 12;
-//num -= 14;
+//num += 12;
+num -= 14;
 //num *= 5;
 //num /= 7;
 //num++;
 //num--;
 alert(num);
 
-alert(num);
