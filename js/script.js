ae2df8723e2d17f616b3fe28645a79b52a296180
PetroViter <116358398+PetroViter@users.noreply.github.com>
2023-03-19 09:53:31 +0200

    +

--- js/script.js
+++ js/script.js
@@ -1,8 +1,5 @@
-// task #1
-console.log(Boolean(Number('10') + 1 )) //true
-console.log('sub ' + Number(false)) //sub 0
-console.log(16 * `     91        `)// 1456
-console.log(true-70)//-69
-console.log(Number(1 + String(1)) + 1) // 12
+'use strict'
 
-
+function number(num) {
+    const num1 = +prompt()
+}
