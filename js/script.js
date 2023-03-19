59f142d9d91810a5475b32271c6dc18cf140f28e
PetroViter <116358398+PetroViter@users.noreply.github.com>
2023-03-19 10:36:07 +0200

    +

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 let x = 4;
 let y = 9;
-let R = 10;
+const R = 10;
 
-let A = Math.sqrt(x*x + y*y)
+const A = Math.sqrt(x*x + y*y)
 
-let dotLocation = A < R ? 'Точка А лежит в окружности' : 'Точка А не лежит в окружности';
+const dotLocation = A < R ? 'Точка А лежит в окружности' : 'Точка А не лежит в окружности';
 
 console.log(dotLocation);
