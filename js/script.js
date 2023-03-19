2fcc5459e153ac65707ac19bb9f5b7c51149df27
PetroViter <116358398+PetroViter@users.noreply.github.com>
2023-03-19 12:25:07 +0200

    +

--- js/script.js
+++ js/script.js
@@ -1,9 +1,23 @@
 'use strict'
-const a = +prompt('Введите первое число');
-const b = +prompt('Введите второе число');
-function number(a, b) {
-    
+// 1
+const a = 5;
+const b = 5;
+function equally(a, b) {
        return a === b;
 }
-number();
+console.log(equally(a, b));
 
+// 2
+const c = 6;
+const d = 6;
+function sum(c, d) {
+       return c + d > 10;
+}
+console.log(sum(c, d));
+
+// 3
+const e = -2;
+function more(e) {
+       return e > 0;
+}
+console.log(more(e));
