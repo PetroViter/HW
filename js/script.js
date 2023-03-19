0641c23dd5e566daf0f608f48ac16bc14d768620
PetroViter <116358398+PetroViter@users.noreply.github.com>
2023-03-19 13:07:33 +0200

    +

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const age = prompt('Сколько Вам лет?')
 const decl = age % 10;
 
-if (age == 11) {
+if (age === 11) {
     alert('Вам ' + age + ' лет')
 } else if (decl === 1) {
     alert('Вам ' + age + ' год')
