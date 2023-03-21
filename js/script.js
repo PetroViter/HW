223cff3397d1124fdebde73466ed76da684e3e98
PetroViter <116358398+PetroViter@users.noreply.github.com>
2023-03-22 00:24:14 +0200

    +

--- js/script.js
+++ js/script.js
@@ -1,8 +1,20 @@
-// task #1
-console.log(Boolean(Number('10') + 1 )) //true
-console.log('sub ' + Number(false)) //sub 0
-console.log(16 * `     91        `)// 1456
-console.log(true-70)//-69
-console.log(Number(1 + String(1)) + 1) // 12
+'use strict'
+const number = +prompt('Введите число');
+const exponent = +prompt('Введите степень');
+function exponentiation (a , extent = 1) {
+
+    if (isNaN(a) || isNaN(extent)) {
+        alert('Вы ввели некорректные данные');
+    } else {
+        return a ** extent;
+    }
+
+}
+
+const operation = exponentiation(number, exponent);
+alert(operation)
+
+
+
 
 
