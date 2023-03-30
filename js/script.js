0cc145c6d2277c9f5081df29b4c49fede97eb746
PetroViter <116358398+PetroViter@users.noreply.github.com>
2023-03-30 12:46:26 +0300

    +

--- js/script.js
+++ js/script.js
@@ -1,8 +1,15 @@
-// task #1
-console.log(Boolean(Number('10') + 1 )) //true
-console.log('sub ' + Number(false)) //sub 0
-console.log(16 * `     91        `)// 1456
-console.log(true-70)//-69
-console.log(Number(1 + String(1)) + 1) // 12
+const fib = (n = 1) => {
+    let a = 0;
+    let b = 1;
+    for (let i = 2; i <= n; i++ ) {
+           let c = a + b;
+           a = b;
+           b = c;
+    }
+           return b;
+}
+console.log(fib(1))
+
+
 
 
