1a850113f5aec6f41b40e2dd25e8488313b1ba31
PetroViter <116358398+PetroViter@users.noreply.github.com>
2023-04-02 07:49:30 +0300

    +

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
-// task #1
-console.log(Boolean(Number('10') + 1 )) //true
-console.log('sub ' + Number(false)) //sub 0
-console.log(16 * `     91        `)// 1456
-console.log(true-70)//-69
-console.log(Number(1 + String(1)) + 1) // 12
+const myFunction = (callback) => {
+    const num = prompt('Enter number')
+    const degree = prompt('Enter degree')
+    callback(num, degree)
+}
 
-
+myFunction(exponentiation = (a, b) => {
+    const result = a ** b
+    alert(result)
+})
