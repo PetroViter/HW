02a6902fecbbe80045bf029172618f439d26075a
PetroViter <116358398+PetroViter@users.noreply.github.com>
2023-04-04 19:04:41 +0300

    +

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-// task #1
-console.log(Boolean(Number('10') + 1 )) //true
-console.log('sub ' + Number(false)) //sub 0
-console.log(16 * `     91        `)// 1456
-console.log(true-70)//-69
-console.log(Number(1 + String(1)) + 1) // 12
-
-
+const divider = prompt('Enter number')
+const smallestDivisor = (num) => {
+    if (num < 1) return NaN
+    for (let i = 2; i < num; i++) {
+        if ( num % i === 0) return i;
+    } return num;
+}
+console.log(smallestDivisor(divider));
