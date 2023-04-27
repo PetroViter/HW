19cfbb0e372cfbc6755c8e1de430614bc86fbf6c
PetroViter <116358398+PetroViter@users.noreply.github.com>
2023-04-27 14:07:17 +0300

    +

--- js/script.js
+++ js/script.js
@@ -1,8 +1,19 @@
-// task #1
-console.log(Boolean(Number('10') + 1 )) //true
-console.log('sub ' + Number(false)) //sub 0
-console.log(16 * `     91        `)// 1456
-console.log(true-70)//-69
-console.log(Number(1 + String(1)) + 1) // 12
+//1
+const getFactorial = n => {
+  if (n === 1) return 1;
+  return n * getFactorial(n - 1);
+}
 
-
+console.log(getFactorial(3))
+//2
+const pow = (x,n) => {
+    if (n !== 1) return  x *= pow(x,n - 1);
+        return x;
+}
+console.log( pow(5,4));
+//3
+const sum = (a, b) => {
+    if (b === 0) return a;
+    return sum(a + 1, b - 1);
+}
+console.log(sum(5,4))
