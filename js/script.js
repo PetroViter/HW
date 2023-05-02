61915b614e212c33e400236fbe61c0ab848ba7dc
PetroViter <116358398+PetroViter@users.noreply.github.com>
2023-05-02 23:35:11 +0300

    +

--- js/script.js
+++ js/script.js
@@ -1,8 +1,6 @@
-// task #1
-console.log(Boolean(Number('10') + 1 )) //true
-console.log('sub ' + Number(false)) //sub 0
-console.log(16 * `     91        `)// 1456
-console.log(true-70)//-69
-console.log(Number(1 + String(1)) + 1) // 12
-
-
+const bind = (fn, context, ...rest) => {
+    return function (...arg) {
+        return fn.call(context, ...rest.concat(arg))
+    }
+}
+bind();
