d3ecaffb53c352438492d630b77d5e74dbe92bb8
PetroViter <116358398+PetroViter@users.noreply.github.com>
2023-05-02 22:15:49 +0300

    +

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
 'use strict'
+const calc = {
+    read () {
+        this.a = +prompt('Ведите число a');
+        this.b = +prompt('Ведите число b');
+    },
+    sum () {
+        return this.a + this.b;
+    },
+    mul () {
+        return this.a * this.b;
+    }
+}
 
-const calc = 
+calc.read();
+alert(calc.sum());
+alert(calc.mul());
+
