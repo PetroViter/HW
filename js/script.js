d9128c89b94ae6f4c8140820d96e874cedffe21f
PetroViter <116358398+PetroViter@users.noreply.github.com>
2023-05-02 20:42:41 +0300

    +

--- js/script.js
+++ js/script.js
@@ -1,8 +1,23 @@
-// task #1
-console.log(Boolean(Number('10') + 1 )) //true
-console.log('sub ' + Number(false)) //sub 0
-console.log(16 * `     91        `)// 1456
-console.log(true-70)//-69
-console.log(Number(1 + String(1)) + 1) // 12
-
+'use strict'
+void function () {
+    const counter = () => {
+        let a = 0;
+        return {
+            increase: () => {
+                return ++a;
+            },
+            value: () => {
+              return a;
+            },
+            decrease: () => {
+                return --a;
+            }
+        }
+    }
+    const count = counter();
+    count.increase();
+    count.increase();
+    count.decrease();
+    count.value();
+}()
 
