9bda63e3352dd318f71dfd493f7ef802fbcbcc04
PetroViter <116358398+PetroViter@users.noreply.github.com>
2023-05-10 10:17:21 +0300

    +

--- js/script.js
+++ js/script.js
@@ -1,8 +1,26 @@
-// task #1
-console.log(Boolean(Number('10') + 1 )) //true
-console.log('sub ' + Number(false)) //sub 0
-console.log(16 * `     91        `)// 1456
-console.log(true-70)//-69
-console.log(Number(1 + String(1)) + 1) // 12
+const ulId = document.querySelector('#ulId');
+const attr = ulId.attributes;
+console.log(attr)
+
+for (let i of attr) {
+    console.log(i);
+}
+
+const attrValueArray = [];
+const attrNameArray = [];
+for (let i = 0; i < attr.length; i++) {
+    attrValueArray[i] = attr[i].value;
+    attrNameArray[i] = attr[i];
+}
+console.log(attrValueArray);
+console.log(attrNameArray);
+
+const li = document.querySelectorAll('li')
+li[9].textContent = 'Привет, меня зовут Петр'
+li[0].setAttribute('data-my-name', 'Petro')
+
+const ul = document.querySelector('ul');
+ul.removeAttribute('data-dog-tail');
+console.log(ul)
 
 
