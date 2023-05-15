c89c83818dc60e61c4f4f3ed76cc4f1ab2b87942
PetroViter <116358398+PetroViter@users.noreply.github.com>
2023-05-15 18:18:55 +0300

    +

--- js/script.js
+++ js/script.js
@@ -1,8 +1,18 @@
-// task #1
-console.log(Boolean(Number('10') + 1 )) //true
-console.log('sub ' + Number(false)) //sub 0
-console.log(16 * `     91        `)// 1456
-console.log(true-70)//-69
-console.log(Number(1 + String(1)) + 1) // 12
+let ladder = {
+    step: 0,
+    up: function() {
+        this.step++;
+        return this;
+    },
+    down: function() {
+        this.step--;
+        return this;
+    },
+    showStep: function() {
+        alert( this.step );
+        return this;
+    }
+};
 
+ladder.up().up().down().showStep();
 
